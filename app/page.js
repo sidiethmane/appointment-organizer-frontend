927377bd0a13f15dcf4007583b8d44e408a09bc8
Yazan-Alaamer12 <yazan.alaamer@informatik.hs-fulda.de>
2024-11-04 23:34:12 +0100

    add button

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import { Datepicker } from "flowbite-react";
+import { Button } from "flowbite-react";
 
 export default function Home() {
   return (
     <div>
       Hello World!
       <Datepicker inline />
+      <Button color="blue">click me </Button>
     </div>
   );
 }
